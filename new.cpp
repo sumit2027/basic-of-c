92e1ce000c1b628bd0ffb0a5c30f3d5fde0a035d
sumit2027 <sk8033743@gmail.com>
2025-09-09 10:49:43 +0530

    this simple program of c++ language

---
+++ new.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cout<<"Enter two numbers\n";
+    cin>>x,y;
+    cout<<"\nThis is a c++ program %d\n",x+y;
+    return 0;
+}
---
+++ new1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    printf("Enter two numbers\n",x,y);
+    scanf("%d %d",&x,&y);
+    printf("Sum of this is = %d",x+y);
+    return 0;
+
+}
